c1d65645d01e72227cd4407126e5a96c0a2cc89c
Jefersson Nathan <admin@phpse.net>
2015-03-03 15:51:42 -0300

    Clousure to include config files :3

--- public/index.php
+++ public/index.php
@@ -2,12 +2,21 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Silex\Application;
 use Application\Controller\Product;
 
-$app = new Silex\Application;
+$app = new Application();
+$loadConfig = function ($dir) use ($app) {
+    $files = new RecursiveDirectoryIterator($dir, FilesystemIterator::SKIP_DOTS);
+    foreach ($files as $fileInfo) {
+        require realpath($fileInfo->getPathname());
+    }
+};
+$loadConfig(__DIR__ . '/../config');
 
 $app['debug'] = true;
 
 $app->get('/', [Product::class, 'add']);
+
 $app->run();
 
