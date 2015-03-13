afab06e91bd974c833bc3d781b94ee8d5eec8a8f
Jefersson Nathan <admin@phpse.net>
2015-03-12 23:01:23 -0300

    rm Controller/Product.php && touch Controllerr/UserController.php

--- src/Application/Controller/UserController.php
+++ src/Application/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace Application\Controller;
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 final class UserController
 {
@@ -23,5 +24,38 @@ final class UserController
     public static function index(Application $app)
     {
         return $app['twig']->render('index.twig');
+    }
+    
+    /**
+     * Try to log a user and redirect them.
+     *
+     * @param Application $app 
+     *
+     * @return string
+     */
+    public static function login(Application $app, Request $request)
+    {
+        $userInfo = [
+            'username' => $request->get('username'),
+            'password' => $request->get('password'),
+        ];
+        
+        if ($user = LoginController::loadUser($userInfo)) {
+            return self::success($app, $user);
+        }
+
+        return self::fail($app, $userInfo);
+    }
+    
+    public static function success(Application $app, $userInfo)
+    {
+        return 'success';
+    }
+    
+    public static function fail(Application $app, $userInfo)
+    {
+        return $app['twig']->render('fail.twig', [
+           'userInfo' => $userInfo, 
+        ]);
     }
 }
