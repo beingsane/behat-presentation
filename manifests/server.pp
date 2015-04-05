a38aff31203875dd453b8ff3e41a1503ce3781c4
Jefersson Nathan <admin@phpse.net>
2015-04-05 04:04:04 -0300

    Use template for config file and create schema/user for database

--- manifests/server.pp
+++ manifests/server.pp
@@ -11,7 +11,7 @@ file { "/etc/mysql/conf.d/allow_external.cnf":
   owner    => mysql,
   group    => mysql,
   mode     => 0644,
-  content  => template("./allow_external.cnf"),
+  content  => template("/vagrant/manifests/allow_external.cnf"),
   require  => Package["mysql-server"],
   notify   => Service["mysql"],
 }
@@ -24,3 +24,27 @@ service { "mysql":
   require    => Package["mysql-server"],
 }
 
+exec { "store-schema":
+  unless  => "mysql -uroot store",
+  command => "mysqladmin -uroot create store",
+  path    => "/usr/bin/",
+  require => Service["mysql"],
+}
+
+exec { "remove-anonymous-user":
+  command => "mysql -uroot -e \"DELETE FROM mysql.user \
+                                WHERE user=''; \
+                                FLUSH PRIVILEGES\"",
+  onlyif  => "mysql -u' '",
+  path    => "/usr/bin",
+  require => Service["mysql"],
+}
+
+exec { "mysql-user":
+  unless   => "mysql -ustore -p123456 store",
+  command  => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON \
+                                 store.* TO 'store'@'%' \
+                                 IDENTIFIED BY '123456';\"",
+  path     => "/usr/bin/",
+  require  => Exec["store-schema"],
+}
